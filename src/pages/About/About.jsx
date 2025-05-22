54fda5109d1f21dfed2f5b5e01ea3c8d7c14f7b7
cdjohnson97 <cdmilandou@gmail.com>
2025-05-22 19:13:24 +0200

    new adding_02

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -21,7 +21,7 @@ const servicesData = [
     description: "Dévopement des applications mobiles iOS et Android en utilisant les derniers outils."
   },
   {
-    icon: "/images/icon-app.svg",
+    icon: "",
     title: "Mes stacks",
     description: (
       <>
@@ -40,6 +40,15 @@ const servicesData = [
             "techLogos/MySQL.svg",
             "techLogos/AWS.svg",
             "techLogos/Docker.svg",
+            "techLogos/Kubernetes.svg",
+            "techLogos/Apache.svg",
+            "techLogos/HashiCorp Terraform.svg",
+            "techLogos/PostgresSQL.svg",
+            "techLogos/IntelliJ IDEA.svg",
+            "techLogos/Python.svg",
+            "techLogos/TypeScript.svg",
+            "techLogos/Linux.svg",
+            "techLogos/JavaScript.svg",
           ].map((img, idx) => (
             <img
               key={idx}
@@ -66,68 +75,62 @@ const About = () => {
   return (
     <article className="about  active" data-page="about">
 
-    <header>
-      <h2 className="h2 article-title">A propos de moi</h2>
-    </header>
+      <header>
+        <h2 className="h2 article-title">A propos de moi</h2>
+      </header>
 
-    <section className="about-text">
-      <p>
-      Développeur Fullstack passionné par les technologies 
-      web modernes et l'automatisation des processus de développement. 
-      Mon expertise technique couvre l'ensemble de la stack de développement,
-       de la conception d'interfaces utilisateur réactives avec React et Next.js jusqu'à l'implémentation
-       de backends robustes en Node.js, Java Spring Boot et Python.
-      </p>
+      <section className="about-text">
+        <p>
+          Développeur Fullstack passionné par les technologies 
+          web modernes et l'automatisation des processus de développement. 
+          Mon expertise technique couvre l'ensemble de la stack de développement,
+          de la conception d'interfaces utilisateur réactives avec React et Next.js jusqu'à l'implémentation
+          de backends robustes en Node.js, Java Spring Boot et Python.
+        </p>
 
-      <p>
-      Ma maîtrise des bases de données relationnelles (MySQL, PostgreSQL) 
-      et NoSQL (MongoDB) me permet de concevoir des architectures de données
-       efficaces et adaptées aux besoins spécifiques de chaque projet. Je développe des API REST performantes et sécurisées, en accordant une attention
-        particulière aux bonnes pratiques et à la qualité du code.
-      </p>
+        <p>
+          Ma maîtrise des bases de données relationnelles (MySQL, PostgreSQL) 
+          et NoSQL (MongoDB) me permet de concevoir des architectures de données
+          efficaces et adaptées aux besoins spécifiques de chaque projet. Je développe des API REST performantes et sécurisées, en accordant une attention
+          particulière aux bonnes pratiques et à la qualité du code.
+        </p>
 
-      <p>
-      Particulièrement attiré par le DevOps et le Cloud Computing,
-       je m'investis dans l'apprentissage et la mise en œuvre de pratiques
-        d'automatisation avancées. Je travaille activement avec Docker pour 
-        la containerisation, Kubernetes pour l'orchestration, et je m'intéresse 
-        de près aux pipelines CI/CD pour optimiser les processus de déploiement.
-       Mon engagement s'étend également vers AWS, où je souhaite approfondir 
-       mes compétences dans l'utilisation des services cloud essentiels comme 
-       EC2 pour le déploiement d'instances, S3 pour le stockage d'objets, RDS pour les bases de données managées, et Lambda pour les fonctions serverless. Mes compétences en administration de serveurs Linux (Ubuntu)
-        et en gestion de services web (Apache) complètent mon profil polyvalent.
-      </p>
-    </section>
+        <p>
+          Particulièrement attiré par le DevOps et le Cloud Computing,
+          je m'investis dans l'apprentissage et la mise en œuvre de pratiques
+          d'automatisation avancées. Je travaille activement avec Docker pour 
+          la containerisation, Kubernetes pour l'orchestration, et je m'intéresse 
+          de près aux pipelines CI/CD pour optimiser les processus de déploiement.
+          Mon engagement s'étend également vers AWS, où je souhaite approfondir 
+          mes compétences dans l'utilisation des services cloud essentiels comme 
+          EC2 pour le déploiement d'instances, S3 pour le stockage d'objets, RDS pour les bases de données managées, et Lambda pour les fonctions serverless. Mes compétences en administration de serveurs Linux (Ubuntu)
+          et en gestion de services web (Apache) complètent mon profil polyvalent.
+        </p>
+      </section>
 
+      {/* <!--
+        - service
+      --> */}
 
-    {/* <!--
-      - service
-        --> */}
+      <section className="service">
 
-    <section className="service">
+        <h3 className="h3 service-title"></h3>
 
-      <h3 className="h3 service-title"></h3>
+        <ul className="service-list">
 
-      <ul className="service-list">
+          {servicesData.map((service, index) => (
+            <li key={index}>
+              <Service
+                icon={service.icon && service.icon.trim() !== "" ? service.icon : undefined}
+                title={service.title}
+                description={service.description}
+              />
+            </li>
+          ))}
+        </ul>
 
-        {servicesData.map((service, index) => (
-          <li key={index}>
-            <Service
-              icon={service.icon}
-              title={service.title}
-              description={service.description}
-            />
-          </li>
-        ))}
-      </ul>
-
-    </section>
-
-
-   
-
-    
-  </article>
+      </section>
+    </article>
   )
 }
 
--- src/pages/About/Service.jsx
+++ src/pages/About/Service.jsx
@@ -3,9 +3,11 @@
 const Service = ({ title, icon, description }) => {
   return (
     <li className="service-item">
-      <div className="service-icon-box">
-        <img src={icon} alt={`${title} icon`} width="40" />
-      </div>
+      {icon && (
+        <div className="service-icon-box">
+          <img src={icon} alt={`${title} icon`} width="40" />
+        </div>
+      )}
       <div className="service-content-box">
         <h4 className="h4 service-item-title">{title}</h4>
         <p className="service-item-text">{description}</p>
